1d0b275433446c5ef6b1e8f5f41b2db798123dbb
Franklin <zerocreative.f@gmail.com>
2019-05-10 03:19:24 -0400

    multilang session

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const Router = require('koa-router')
 const loadRoutes = require("./app/routes")
 const views = require('koa-views')
 const serve = require('koa-static')
-const session = require('koa-session');
-const database = require('./app/database/db.js');
+const session = require('koa-session')
+const database = require('./app/database/db.js')
 const interface = require('./app/interface/interface.js')
 
 const app = new Koa()
@@ -15,6 +15,8 @@ const router = new Router()
 app.keys = [config.get("key")];
 
 app.use(session(app));
+
+
 
 
 // Views setup, adds render() function to ctx object
@@ -28,9 +30,18 @@ app.use(serve(config.get('static.path')))
 
 // Hydrate ctx.state with global settings, to make them available in views
 app.use(async (ctx, next) => {
+    
+    if (!ctx.session.lang){ // checking accepted lang (if not defined) from user browser to set default lang
+        var languagesAccepted = ctx.request.header['accept-language'].match(/[a-zA-Z\-]{2,10}/g) || [];
+        if (languagesAccepted.includes("es") || languagesAccepted.includes("es-ES"))
+            ctx.session.lang="es"
+        else ctx.session.lang="en"
+    }
+
 	ctx.state.settings = config.get('settings')
     ctx.state.database = database.connection(config.get('database'));
 	ctx.state.urlWithoutQuery = ctx.origin + ctx.path
+
 	await next()
 })
 
