bfab26322ba5d3ee7420565b5dd5d571dc698256
Franklin <zerocreative.f@gmail.com>
2019-05-18 22:00:21 -0400

    home, end keys action + comments

--- app/assets/scripts/siteEvents.js
+++ app/assets/scripts/siteEvents.js
@@ -1,5 +1,9 @@
-// *****
-// Explore thhrough to the .scrollto anchor elements and add the listener
+// Update the previousScrollPosition whenever scroll
+document.querySelector('body').addEventListener('scroll', function(event){
+  previousScrollPosition=this.scrollTop  
+});
+
+// Explore thhrough all the .scrollto anchor elements and add listeners
 for (let elem of document.querySelectorAll('.scrollto')) {
   
   elem.addEventListener('click',function(e) {
@@ -12,6 +16,7 @@ for (let elem of document.querySelectorAll('.scrollto')) {
       window.history.pushState("object or string", "Title", "#"+targetSection);
       document.querySelector('body').scroll({left: 0, top: document.querySelector('#'+targetSection).offsetTop , behavior: 'smooth'});
 
+      // Move menu outside screen
       move("header .logo-container a").set("margin-left",-document.querySelector("body").offsetWidth * 0.41).duration('0.2s').end()
       move("header .rois-logo-line").set("margin-left",-document.querySelector("body").offsetWidth * 0.41).duration('0.2s').end()
       move("header .rois-logo-dot").set("margin-left",-document.querySelector("body").offsetWidth * 0.41).duration('0.2s').end()
@@ -20,17 +25,19 @@ for (let elem of document.querySelectorAll('.scrollto')) {
       move("header .rois-menu-line-thick").set("margin-left",document.querySelector("body").offsetWidth * 0.6).duration('0.2s').end()
       move("header .rois-menu-line-thin").set("margin-left",document.querySelector("body").offsetWidth * 0.6).duration('0.2s').end()
 
-
       setTimeout(function() {
         var activeSection = getActiveSectionIndex(); 
 
+        // Move header to target section
         document.querySelector('header.header-container').style.top=document.querySelector('#'+targetSection).offsetTop
 
+        
+
+        // Move menu inside screen again
         move("header .logo-container a").set("margin-left",0).duration('0.2s').end()
         move("header .rois-logo-line").set("margin-left",0).duration('0.2s').end()
         move("header .rois-logo-dot").set("margin-left",0).duration('0.2s').end()
         move("header .secundary-rois-container").set("margin-left",0).duration('0.2s').end()
-
         setTimeout(function() {
           move("header #header_menu").set("margin-left",0).duration('0.4s').end()
           move("header .rois-menu-line-thick").set("margin-left",0).duration('0.4s').end()
@@ -44,20 +51,29 @@ for (let elem of document.querySelectorAll('.scrollto')) {
 
 }
 
-
-document.querySelector('body').addEventListener("keyup", function(e) {
-  e.preventDefault();
-});
-document.querySelector('body').addEventListener("keypress", function(e) {
-  e.preventDefault();
-});
+// Disable events froms keyboard
+document.querySelector('body').addEventListener("keyup", function(e) {e.preventDefault();});
+document.querySelector('body').addEventListener("keypress", function(e) {e.preventDefault();});
 
 
+// Enable keydown from these navigation keys only to trigger click on respective anchor link
 document.querySelector('body').addEventListener("keydown", function(e) {
     var activeSection = getActiveSectionIndex();
-    // home, end, lef, right keys:
-    if([/*35,36*/,37,39].indexOf(e.keyCode) > -1) {
+    // lef, right keys:
+    if([37,39].indexOf(e.keyCode) > -1) {
       e.preventDefault();
+    }
+    // home keys
+    else if([35].indexOf(e.keyCode) > -1) {
+      e.preventDefault();
+      targetSection=document.querySelectorAll('Section')[0].getAttribute("id")
+      simulateClick(document.querySelector(".scrollto[href='#"+targetSection+"']"))
+    }
+    // end keys
+    else if([36].indexOf(e.keyCode) > -1) {
+      e.preventDefault();
+      targetSection=document.querySelectorAll('Section')[document.querySelectorAll('Section').length -1].getAttribute("id")
+      simulateClick(document.querySelector(".scrollto[href='#"+targetSection+"']"))
     }
     // space, page down, down keys:
     else if([32, 34, 40].indexOf(e.keyCode) > -1) {
@@ -65,8 +81,6 @@ document.querySelector('body').addEventListener("keydown", function(e) {
       if (activeSection< document.querySelectorAll('Section').length -1 ){ //  down
         targetSection=document.querySelectorAll('Section')[activeSection+1].getAttribute("id")
         simulateClick(document.querySelector(".scrollto[href='#"+targetSection+"']"))
- 
-
       }
     }
     // page up, up keys:
@@ -82,6 +96,7 @@ document.querySelector('body').addEventListener("keydown", function(e) {
 }, false);
 
 
+// Whenever wheel trigger click on respective anchor link
 document.querySelector('body').addEventListener('wheel', function(e) {  
     if (!scrolling){
       scrolling=1;
@@ -105,7 +120,4 @@ document.querySelector('body').addEventListener('wheel', function(e) {
 },{ passive: false });
 
 
-document.querySelector('body').addEventListener('scroll', function(event){
-  previousScrollPosition=this.scrollTop  
-});
 
